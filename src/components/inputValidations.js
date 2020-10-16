5e8e0fb38cc11b69ef5f71fdf00b937f7808d490
EveFon Wu <evefonwu@gmail.com>
2020-10-15 22:16:52 -0400

    Fix msg value into setMessage

--- src/components/inputValidations.js
+++ src/components/inputValidations.js
@@ -10,13 +10,10 @@ export function messageWith(structureErrors, setMessage) {
   if (value === undefined) {
     msg[key] = `${key} required`;
   } else if (type === "never") {
-    msg[key] = `attribute_unknown`;
+    msg[key] = `attribute unknown`;
   } else {
     msg[key] = `${key} is invalid`;
   }
   console.log(msg);
-  setMessage({
-    ...msg,
-    msg,
-  });
+  setMessage(msg);
 }
